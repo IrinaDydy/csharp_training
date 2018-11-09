847f53565902ad677e5da8357241486081e21378
dydyshko_i <irina.dydyshko@kaspersky.com>
2018-11-10 00:18:12 +0300

    - Добавлен базовый класс - От базового класса унаследованы тестовые классы - Все вспомогательные методы перенесены в базовый класс - Добавлен тест для удаления группы

---
+++ addressbook-web-tests/addressbook-web-tests/RemoveGroupTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class RemoveGroupTests:TestBase
+    {
+        [Test]
+        public void RemoveGroupTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupsPage();
+            Logout();
+        }
+    }
+}
